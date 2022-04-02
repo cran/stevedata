970785d0522427b5e2ca4498ec80d7f59c0376df
Steve Miller <steven.v.miller@gmail.com>
2022-04-02 17:20:02 +0000

    version 0.7.0

--- R/rd-steves_clothes.R
+++ R/rd-steves_clothes.R
@@ -1,13 +1,13 @@
 #' @importFrom tibble tibble
 NULL
 
-#' Steve's (Professional) Clothes, as of March 3, 2019
+#' Steve's (Professional) Clothes, as of March 20, 2022
 #'
 #' I cobbled together this data set of the professional clothes
 #'  (polos, long-sleeve dress shirts, pants) in my closet, largely for
 #'  illustration on the origins of apparel in the U.S. for an intro lecture on trade.
 #'
-#' @format A data frame with 79 observations on the following 4 variables.
+#' @format A data frame with 86 observations on the following 4 variables.
 #' \describe{
 #' \item{\code{type}}{Type of clothing}
 #' \item{\code{brand}}{The brand of clothing (e.g. Apt. 9, Saddlebred)}
@@ -16,8 +16,11 @@ NULL
 #' }
 #'
 #' @details If you must know, I do most of my clothes shopping at major retailers
-#' in the U.S. (mostly Belk, J.C. Penney, and Kohl's). If that's you as well, the
-#' odds are good the distribution of my clothes will closely resemble yours.)
+#' in the U.S. Thi is mostly Belk, J.C. Penney, and Kohl's. If that's you as well, the
+#' odds are good the distribution of my clothes will closely resemble yours. A recent
+#' move I made resulted in me donating a fair bit of my short-sleeved polo shirts. I did
+#' not buy any new shirts, though. Thus, I copied that information from a previous version
+#' of the data.
 #'
 #' @source Steve's closet. Hey, that's me!
 #'
